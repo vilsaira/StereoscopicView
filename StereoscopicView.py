2fe3617f4ca0d6af5b16a258288a4ce0922cf192
vilsaira <viljami.sairanen@gmail.com>
2020-04-24 10:52:23 +0300

    update

--- StereoscopicView.py
+++ StereoscopicView.py
@@ -23,44 +23,44 @@ eyedist = 50
 actors =  rendererL.GetActors()
 actors.InitTraversal()
 for i in range(0, actors.GetNumberOfItems()):
-  actor = actors.GetNextActor()
-  if actor is not None:
-    rendererR.AddActor(actor) 
+    actor = actors.GetNextActor()
+    if actor is not None:
+        rendererR.AddActor(actor) 
 
 volumes = rendererL.GetVolumes()
 volumes.InitTraversal()
 for i in range(0, volumes.GetNumberOfItems()):
-  item = volumes.GetNextItem()
-  if actor is not None:    
-    rendererR.AddVolume(item)    
+    item = volumes.GetNextItem()
+    if actor is not None:
+        rendererR.AddVolume(item)    
 
 props = rendererL.GetViewProps()
 props.InitTraversal()
 for i in range(0, props.GetNumberOfItems()):
-  prop = props.GetNextProp()
-  if prop is not None:
-    rendererR.AddViewProp(prop)
+    prop = props.GetNextProp()
+    if prop is not None:
+        rendererR.AddViewProp(prop)
 	
 def onMouseMove(observer,eventid):
-  updateCameraPositions()
-  camera1.SetFocalPoint(camera0.GetFocalPoint())
-  camera1.SetDistance(camera0.GetDistance())
-  camera1.SetRoll(camera0.GetRoll())
-  camera1.SetViewUp(camera0.GetViewUp())
+    updateCameraPositions()
+    camera1.SetFocalPoint(camera0.GetFocalPoint())
+    camera1.SetDistance(camera0.GetDistance())
+    camera1.SetRoll(camera0.GetRoll())
+    camera1.SetViewUp(camera0.GetViewUp())
   
   
 def updateCameraPositions():
-  pos = camera0.GetPosition()
-  pos2 = [pos[0] - 65, pos[1], pos[2]]
-  camera1.SetPosition(pos2)
-  rendererL.ResetCameraClippingRange()
-  rendererR.ResetCameraClippingRange()
+    pos = camera0.GetPosition()
+    pos2 = [pos[0] - 65, pos[1], pos[2]]
+    camera1.SetPosition(pos2)
+    rendererL.ResetCameraClippingRange()
+    rendererR.ResetCameraClippingRange()
   
  
  
 camera0.AddObserver(vtk.vtkCommand.ModifiedEvent, onMouseMove) 
 
-## slider to adjust spacing between eyes
+# slider to adjust spacing between eyes
 slider = qt.QSlider()
 slider.setOrientation(1) # horizontal slider
 slider.setMinimum(0)
